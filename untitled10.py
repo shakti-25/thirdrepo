592add6557888dc921497886aa5ab41ddf0d105a
shakti-25 <thevarshakti02@gmail.com>
2024-12-14 17:17:08 +0530

    Update untitled10.py

--- untitled10.py
+++ untitled10.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.metrics.pairwise import cosine_similarity
 import ast
 
